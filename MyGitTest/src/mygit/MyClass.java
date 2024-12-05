dfeda7aa097794b99ff1b288e5b8daa320c123ed
ohige01 <ohige01@naver.com>
2024-12-05 13:54:51 +0900

    Change in master branch

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd Version!");
+		System.out.println("My changed in master branch");
 	}
 }
