cd749671c9e45e76845126dd3585267cf3d34f72
ohige01 <ohige01@naver.com>
2024-12-15 15:50:20 +0900

    프로젝트 시작(과제)

--- MyGitTest/src/mygit/YourClass.java
+++
@@ -1,9 +0,0 @@
-package mygit;
-
-public class YourClass {
-	public void sayHello(String toWhom) {
-		System.out.println("Hello!" + toWhom);
-		System.out.println("Your changed in master branch");
-	}
-
-}
