03753ecc933d93c9cceeaa280c48e6c40abb2c66
ohige01 <ohige01@naver.com>
2024-12-15 15:50:20 +0900

    edit branch devC

---
+++ MyGitTest/src/mygit/A.java
@@ -0,0 +1,11 @@
+package mygit;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("init A.java");
+		System.out.println("edit branch devC");
+	}
+
+}
---
+++ MyGitTest/src/mygit/C.java
@@ -0,0 +1,11 @@
+package mygit;
+
+public class C {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("init C.java");
+		System.out.println("edit branch devC");
+	}
+
+}
