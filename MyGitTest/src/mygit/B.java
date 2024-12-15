96050c2585358d9e771023ef99a6c2233c6f3010
ohige01 <ohige01@naver.com>
2024-12-15 15:53:56 +0900

    edit branch devB

--- MyGitTest/src/mygit/B.java
+++ MyGitTest/src/mygit/B.java
@@ -5,6 +5,7 @@ public class B {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("init B.java");
+		System.out.println("edit branch devB");
 	}
 
 }
